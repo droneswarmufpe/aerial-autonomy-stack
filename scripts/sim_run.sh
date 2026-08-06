4f680394808ce5fd1620a18cbcbf0cd5cfabc5fe
jgocm <jgocm@cin.ufpe.br>
2026-08-06 13:49:36 -0300

    remove patches volume from ground container

--- scripts/sim_run.sh
+++ scripts/sim_run.sh
@@ -158,7 +158,6 @@ if [[ "$HITL" == "false" ]]; then
     DOCKER_CMD="docker run -it --rm \
       --ipc=host \
       --volume ${PARENT_DIR}/github_clones/Projeto-Enxame-Drones:/aas/Projeto-Enxame-Drones \
-      --volume ${PARENT_DIR}/simulation/simulation_resources/patches:/aas/custom_objects \
       --volume /tmp/.X11-unix:/tmp/.X11-unix:rw --device /dev/dri --gpus all \
       --env DISPLAY=$DISPLAY --env QT_X11_NO_MITSHM=1 --env NVIDIA_DRIVER_CAPABILITIES=all --env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR --env GST_DEBUG=3 \
       --env HEADLESS=$HEADLESS --env RCPILOT=$RCPILOT \
