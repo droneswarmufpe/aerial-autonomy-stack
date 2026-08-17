1e3d1f9a868c532bc8f1e4fe899e7ed964e46da3
llucasEmanuel <lluukkaass62@gmail.com>
2026-08-17 15:32:30 -0300

    fix: update custom objects path

--- scripts/sim_run.sh
+++ scripts/sim_run.sh
@@ -158,14 +158,11 @@ if [[ "$HITL" == "false" ]]; then
     DOCKER_CMD="docker run -it --rm \
       --ipc=host \
       --volume ${PARENT_DIR}/github_clones/Projeto-Enxame-Drones:/aas/Projeto-Enxame-Drones \
-<<<<<<< HEAD
-      --volume ${PARENT_DIR}/simulation/simulation_resources/patches:/aas/custom_objects \
       --volume ${PARENT_DIR}/github_clones/rcpilot:/aas/rcpilot \
       --volume /aas/rcpilot/build \
       --volume /aas/rcpilot/install \
       --volume /aas/rcpilot/log \
-=======
->>>>>>> 4f680394808ce5fd1620a18cbcbf0cd5cfabc5fe
+      --volume ${PARENT_DIR}/simulation/simulation_resources/custom_objects:/aas/custom_objects \
       --volume /tmp/.X11-unix:/tmp/.X11-unix:rw --device /dev/dri --gpus all \
       --env DISPLAY=$DISPLAY --env QT_X11_NO_MITSHM=1 --env NVIDIA_DRIVER_CAPABILITIES=all --env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR --env GST_DEBUG=3 \
       --env HEADLESS=$HEADLESS --env RCPILOT=$RCPILOT \
@@ -203,7 +200,7 @@ if [[ "$HITL" == "false" ]]; then
       DOCKER_CMD="docker run -it --rm \
         --ipc=host \
         --volume ${PARENT_DIR}/github_clones/Projeto-Enxame-Drones:/aas/Projeto-Enxame-Drones \
-        --volume ${PARENT_DIR}/simulation/simulation_resources/patches:/aas/custom_objects \
+        --volume ${PARENT_DIR}/simulation/simulation_resources/custom_objects:/aas/custom_objects \
         --volume ${PARENT_DIR}/github_clones/rcpilot:/aas/rcpilot \
         --volume /aas/rcpilot/build \
         --volume /aas/rcpilot/install \
